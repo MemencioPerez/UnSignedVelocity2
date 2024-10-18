057a60814b58bd7cfc31a2e1893734f62a9adf74
Memencio <memencio.perez.2004@gmail.com>
2024-10-18 17:48:27 -0400

    fix: Update PacketEvents API to v2.5.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val packetEventsVersion = libs.versions.packetevents.get()
 repositories {
     maven("https://papermc.io/repo/repository/maven-public/")
     maven("https://repo.william278.net/velocity/")
-    maven("https://repo.codemc.io/repository/maven-${ if (packetEventsVersion.contains("SNAPSHOT")) "snapshots" else "releases"}/")
+    maven("https://repo.codemc.io/repository/maven-snapshots/")
 }
 
 dependencies {
