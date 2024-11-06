fd0d758468686bad2fafde0528935b0e11aac9c5
Memencio <memencio.perez.2004@gmail.com>
2024-11-06 14:22:16 -0400

    fix: Update CodeMC.io repo link to get PacketEvents 2.6.0 artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val packetEventsVersion = libs.versions.packetevents.get()
 repositories {
     maven("https://papermc.io/repo/repository/maven-public/")
     maven("https://repo.william278.net/velocity/")
-    maven("https://repo.codemc.io/repository/maven-snapshots/")
+    maven("https://repo.codemc.io/repository/maven-releases/")
 }
 
 dependencies {
