d34d23359c5f6b6f233006cd73921143475a2277
Memencio <memencio.perez.2004@gmail.com>
2024-11-07 03:03:20 -0400

    misc: Delete ClientVersionUtil

--- src/main/java/io/github/_4drian3d/unsignedvelocity/listener/packet/login/LoginListener.java
+++ src/main/java/io/github/_4drian3d/unsignedvelocity/listener/packet/login/LoginListener.java
@@ -5,8 +5,10 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
+import com.github.retrooper.packetevents.manager.server.VersionComparison;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.packettype.PacketTypeCommon;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.protocol.player.User;
 import com.github.retrooper.packetevents.util.crypto.MinecraftEncryptionUtil;
 import com.github.retrooper.packetevents.wrapper.login.client.WrapperLoginClientEncryptionResponse;
@@ -17,7 +19,6 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.config.ProxyConfig;
 import io.github._4drian3d.unsignedvelocity.configuration.Configuration;
 import io.github._4drian3d.unsignedvelocity.listener.packet.ConfigurablePacketListener;
-import io.github._4drian3d.unsignedvelocity.utils.ClientVersionUtil;
 
 import java.security.PublicKey;
 import java.util.concurrent.TimeUnit;
@@ -55,7 +56,7 @@ public final class LoginListener extends ConfigurablePacketListener {
         final User user = event.getUser();
         final PacketTypeCommon packetType = event.getPacketType();
         if (packetType == PacketType.Login.Client.LOGIN_START) {
-            if (ClientVersionUtil.doesNotEnforceKeyAuthentication(user)) {
+            if (!user.getClientVersion().is(VersionComparison.EQUALS, ClientVersion.V_1_19) && !user.getClientVersion().is(VersionComparison.EQUALS, ClientVersion.V_1_19_1)) {
                 return;
             }
 
@@ -67,7 +68,7 @@ public final class LoginListener extends ConfigurablePacketListener {
             packet.setSignatureData(null);
             event.markForReEncode(true);
         } else if (packetType == PacketType.Login.Client.ENCRYPTION_RESPONSE) {
-            if (ClientVersionUtil.doesNotEnforceKeyAuthentication(user)) {
+            if (!user.getClientVersion().is(VersionComparison.EQUALS, ClientVersion.V_1_19) && !user.getClientVersion().is(VersionComparison.EQUALS, ClientVersion.V_1_19_1)) {
                 return;
             }
 
@@ -91,7 +92,7 @@ public final class LoginListener extends ConfigurablePacketListener {
         final User user = event.getUser();
         final PacketTypeCommon packetType = event.getPacketType();
         if (packetType == PacketType.Login.Server.ENCRYPTION_REQUEST) {
-            if (ClientVersionUtil.doesNotEnforceKeyAuthentication(user)) {
+            if (!user.getClientVersion().is(VersionComparison.EQUALS, ClientVersion.V_1_19) && !user.getClientVersion().is(VersionComparison.EQUALS, ClientVersion.V_1_19_1)) {
                 return;
             }
             WrapperLoginServerEncryptionRequest packet = new WrapperLoginServerEncryptionRequest(event);
